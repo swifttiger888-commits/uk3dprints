7101feac5534fec2ebded250422812b555067b4e
root <root@vultr.guest>
2026-06-15 14:39:36 +0000

    debug: minimal React test to isolate blank page

---
+++ public/check.js
@@ -0,0 +1,2 @@
+console.log('check.js loaded');
+document.getElementById('root').innerHTML = '<div style="color:#00FF88;background:#0A0A0A;padding:2rem;font-family:sans-serif;text-align:center"><h1>JavaScript is working!</h1><p>If you see this, JS executes fine.</p></div>';
